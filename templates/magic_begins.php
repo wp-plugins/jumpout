0e100dfa42add2dda13294e2f7c84cc2cae7cedf
TheGP <TheGP@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-11 13:50:39 +0000

    magicbegins support
    git-svn-id: https://plugins.svn.wordpress.org/jumpout/trunk@1005650 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/magic_begins.php
@@ -0,0 +1,77 @@
+<style type="text/css">
+.sociallocker div {
+    margin-bottom: 6px;
+}
+.sociallocker small {
+    display: block;
+    color: gray;
+    margin-top: 0px;
+    line-height: 7px;
+}
+</style>
+
+<form action="?page=jumpout&action=magic_begins&noheader=true" method="POST" class="">
+    <p>
+        Описание этого скрипта, вы можете найти по
+        <a href="http://jumpout.makedreamprofits.ru/bonuses/" target="_blank">этой ссылке</a>.
+        После включения, плагин автоматически вставит его на все страницы вашего сайта.
+    </p>
+
+    <p>
+        <label>
+            <input type="checkbox" name="data[enabled]" <?php echo (isset($data['magic_begins']) && isset($data['magic_begins']['enabled']) && TRUE === $data['magic_begins']['enabled']) ? 'checked="checked"' : ''?> /> Включить скрипт
+        </label>
+    </p>
+    <span id="autofill" style="display: none;">
+        <p>
+            <label>
+                <input type="checkbox" name="data[autofill]" <?php echo (isset($data['magic_begins']) && isset($data['magic_begins']['autofill']) && TRUE === $data['magic_begins']['autofill']) ? 'checked="checked"' : ''?> /> Включить автозаполнение форм подписки, когда есть данные подписчика
+            </label>
+        </p>
+        <p>
+            <label>
+                <input type="checkbox" name="data[async]" <?php echo (isset($data['magic_begins']) && isset($data['magic_begins']['autofill']) && TRUE === $data['magic_begins']['async']) ? 'checked="checked"' : ''?> /> Асинхронная загрузка (не задерживает загрузку вашего сайта, рекомендуется)
+            </label>
+        </p>
+    </span>
+    <!--
+    Код Google Analytics:<br />
+    <textarea name="data[magic_begins_code]" style="width: 500px; height: 300px;"><?php echo $data; ?></textarea>
+    <small>будет вставлен между тегами &lt;head&gt; и &lt;/head&gt; на всех страницах</small>
+-->
+
+    <input class="button-primary" type="submit" name="item_add" value="Сохранить" style="margin: 5px 0 5px 8px;" />
+
+
+
+    <input type="hidden" name="action" value="magic_begins" />
+
+</form>
+
+
+<script>
+jQuery(document).ready(function(){
+    jQuery('input[name=data\\[enabled\\]]').change(function(){
+
+        script_settings_visibility();
+
+    });
+
+    script_settings_visibility();
+});
+
+function script_settings_visibility() {
+
+    if (jQuery('input[name=data\\[enabled\\]]').is(':checked')) {
+        jQuery('#autofill').show();
+    } else {
+        jQuery('#autofill').hide();
+    }
+    
+
+}
+</script>
+
+
+
+<?php 
